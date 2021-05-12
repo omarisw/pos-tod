13bf1983fcd6e51016bbf754f0fdd4f241e0eb9d
Omar Mejia <topesa@gmail.com>
2021-05-12 10:25:08 -0700

    [ADD] hw_drivers_extension : se agrega modulo de drivers para iot

---
+++ hw_drivers_extension/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'TOD Drivers Extension',
+    'version': '14.0.0.0',
+    'summary': 'Este modulo extiende los controladores para el punto de venta',
+    'description': 'Extiende el modulo de hw_drivers para el punto de venta',
+    'category': 'General',
+    'author': 'TOD SAS',
+    'website': 'www.tod.com.mx',
+    'license': 'AGPL-3',
+    'depends': ['hw_drivers'],
+    'data': [
+    ],
+    'installable': True,
+}
