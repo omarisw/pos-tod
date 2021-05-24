a8bdddf1d624250e91489cf0645c595c7575e0f7
Omar Mejia <topesa@gmail.com>
2021-05-24 14:59:55 -0700

    [ADD] tod_business_rules

---
+++ tod_business_rules/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "TOD BUSINESS RULES",
+    "author": "TOD SA DE CV, ",
+    "version": "14.0.0.0",
+    "category": "General",
+    "depends": ['sale', 'account', 'purchase', 'stock'],
+    "data": [
+        'views/rules_views.xml'
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
